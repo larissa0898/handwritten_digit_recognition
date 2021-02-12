f6337a93966e21540e0431d2bbc35f2374cc740f
larissa0898 <larissa0898mx.de>
2021-02-12 18:33:14 +0100

    encoding und embedding von labeln und images + label als liste in tupel, sowie angefangen model zu erstellen

--- dl_project/data.py
+++ dl_project/data.py
@@ -1,22 +1,10 @@
 import numpy as np
-import torch
-import random
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-import idx2numpy
-import matplotlib.pyplot as plt
 import gzip
-
-####################################################
-# to print images
-###################################################
-#file1 = 'train-images.idx3-ubyte'
-#imagearray = idx2numpy.convert_from_file(file1)
-
-#plt.imshow(imagearray[1], cmap=plt.cm.binary)
-#plt.show()
-#########################################################
+import torch
+import torch.nn as nn
+#import torchvision
+#import torchvision.transforms as transforms
+#from torch.utils.data import DataLoader
 
 #########################################################################
 # images into numpy array
@@ -39,7 +27,17 @@ def training_images():
             .reshape((image_count, row_count, column_count))
         return images
 
-#print(type(training_images()))             <---- return type: numpy array
+
+new_images = []
+
+for image in training_images():                                    # image pixel from 0-255 to 0.0-1.0
+    for pixel in image:
+        new_images.append(pixel/255.0)
+
+new_images = np.array(new_images)                                 # new_images von Liste in numpy array umwandeln
+
+image_tensor = torch.from_numpy(new_images)                       # numpy array zu Tensor umwandeln
+image_tensor = torch.reshape(image_tensor, (60000, 28, 28))       # Tensor in richtige Form bringen
 
 
 ######################################################################
@@ -59,21 +57,42 @@ def training_labels():
         return labels
 
 labels_tensor = torch.from_numpy(training_labels())                       # numpy-array of labels into torch.tensor of labels
+labels_tensor = labels_tensor.long()                                      # into long values for one-hot encoding
+
+###########################################################################################
+# create new data set with tuples; Liste mit Tupeln drin [(image1, label1), (image2, label2),...]
+###########################################################################################
+data = []
+
+for i in range(len(training_labels())):
+    data.append((image_tensor[i], labels_tensor[i]))
+
+
+#############################################################################################
+# image encoding
+############################################################################################
+
+
+# ?
+
 
 
 #########################################################################################
-# one-hot_encoding of labels
+# one-hot_encoding of labels and label embedding
 #########################################################################################
-x = torch.nn.functional.one_hot(labels_tensor.long(), num_classes=10)
-result = torch.reshape(x, (600,100,10))
+enc_data = []
+E = torch.randn(10)
+
+for image, label in data:
+    enc_label = torch.nn.functional.one_hot(label, num_classes=10)
+    enc_label = enc_label.float()*E
+    enc_data.append((image, enc_label))
+
 
 #stacked_tensor = torch.stack(listoftensors)                                   # falls alle Labels in einen einzigen Tensor rein sollen und nicht in
 #stacked_tensor = torch.reshape(stacked_tensor, (60000,10))                    # 600 verschiedenen
 
-
-
-
-""" 
+"""
 transformer = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
@@ -82,7 +101,7 @@ transformer = transforms.Compose([
 
 # DataLoaders
 batch_size=100
-train_path = 'C:\\Users\\laris\\Desktop\\dl_project\\data'
+train_set = 'C:\\Users\\laris\\Desktop\\dl_project\\data'
 test_path = 'C:\\Users\\laris\\Desktop\\dl_project\\data'
 
 train_loader = DataLoader(
@@ -118,3 +137,16 @@ for batch_size, (_, y) in enumerate(train_loader):
 #stacked_tensor = torch.stack(listoftensors)                                   # falls alle Labels in einen einzigen Tensor rein sollen und nicht in
 #stacked_tensor = torch.reshape(stacked_tensor, (60000,10))                    # 600 verschiedenen
  """
+
+
+########################################################
+# create model
+########################################################
+
+#model = nn.Sequential(nn.Linear(input_size, hidden_size1),
+                       # nn.ReLU()
+                        #nn.Linear(hidden_size1, hidden_size2),
+                        #nn.ReLU(),
+                        #nn.Linear(hidden_size2, output_size),
+                        #nn.LogSoftmax(dim=1)
+                        #)
